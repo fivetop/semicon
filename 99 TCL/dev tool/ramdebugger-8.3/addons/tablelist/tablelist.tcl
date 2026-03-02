5e5af667168995ab7b2f04461d68ac654ae3f949
romee <romyoh@gmail.com>
2026-03-02 12:48:36 +0900

    tclpro

---
+++ 99 TCL/dev tool/ramdebugger-8.3/addons/tablelist/tablelist.tcl
@@ -0,0 +1,14 @@
+#==============================================================================
+# Main Tablelist package module.
+#
+# Copyright (c) 2000-2008  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+#==============================================================================
+
+package require Tcl 8
+package require Tk  8
+package require -exact tablelist::common 4.9.1
+
+package provide Tablelist $::tablelist::version
+package provide tablelist $::tablelist::version
+
+::tablelist::useTile 0
